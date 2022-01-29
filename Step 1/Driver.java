88e564170a7a4edbbd45ef5543d8129ee39bf39c
Derrick <derrick.palma@gmail.com>
2022-01-29 07:00:00 -0800

    Added previous changes to merge

--- Step 1/Driver.java
+++ Step 1/Driver.java
@@ -9,18 +9,19 @@ import java.io.*;
 
 public class Driver {
     public static void main(String[] args) throws Exception {
-       
-        //System.out.println("Parsing: " + args[0]); // Parsing: (name of file being parsed.micro)
 
+        //System.out.println("Parsing: " + args[0]); // Parsing: (name of file being parsed.micro)
         InputStream in = System.in;
         ANTLRInputStream input = new ANTLRInputStream(in);
+
         Little lexer = new Little(input); // this must match antlr_grammar.g4 file name
         Token token = lexer.nextToken();
-	    Vocabulary token2 = lexer.getVocabulary();
+        Vocabulary token2 = lexer.getVocabulary();
 
         while(token.getType() != token.EOF){ // while not end of .g4 file -> get token type
+
             //System.out.println("\t" + getTokenType(token.getType()) + "\t\t" + token.getText());
-	        System.out.println("Token Type: " + token2.getSymbolicName(token.getType()) + "\nValue: " + token.getText());
+            System.out.println("Token Type: " + token2.getSymbolicName(token.getType()) + "\nValue: " + token.getText());
             token = lexer.nextToken();
         }
     }
