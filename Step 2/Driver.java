a0119b05975cab9fd570f3ecbe13c44ac6626515
Bradley Crutchfield <n01441416@unf.edu>
2022-02-19 15:34:59 -0500

    Driver Update for Step 2

--- Step 2/Driver.java
+++ Step 2/Driver.java
@@ -5,6 +5,7 @@
  * @version <January 30, 2022>
  */
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 import java.io.*;
 
 public class Driver {
@@ -13,15 +14,25 @@ public class Driver {
         //System.out.println("Parsing: " + args[0]); // Parsing: (name of file being parsed.micro)
 		
         InputStream in = System.in; // The InputStream represents an input stream of bytes. Can be used to read data.
-	    ANTLRInputStream input = new ANTLRInputStream(in); // Vacuum all input from a Reader/InputStream and then treat it like a char[] buffer
-        Little lexer = new Little(input); // this must match antlr_grammar.g4 file name, will use g4 grammar
-        Token token = lexer.nextToken();
-        Vocabulary vocab = lexer.getVocabulary();
+	ANTLRInputStream input = new ANTLRInputStream(in); // Vacuum all input from a Reader/InputStream and then treat it like a char[] buffer
+        LittleLexer lexer = new LittleLexer(input); // this must match antlr_grammar.g4 file name, will use g4 grammar
+        CommonTokenStream cts = new CommonTokenStream(lexer);
+	LittleParser parser = new LittleParser(cts);
+	parser.program();
 
-        while(token.getType() != token.EOF){ // while not end of file -> get token type
+	if(parser.getNumberOfSyntaxErrors() == 0){
+		System.out.println("Accepted");
+	}
+	else{
+		System.out.println("Not Accepted");
+	}
+        //Token token = lexer.nextToken();
+        //Vocabulary vocab = lexer.getVocabulary();
+
+        /*while(token.getType() != token.EOF){ // while not end of file -> get token type
             System.out.println("Token Type: " + vocab.getSymbolicName(token.getType()) + "\nValue: " + token.getText());
             token = lexer.nextToken();
-        }
+        }*/
     }
 }
 
